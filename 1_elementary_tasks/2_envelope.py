165dd0415565d17f58f130937fdd86d97d00aa29
melancholiaque <melancholique655312@gmail.com>
2019-01-30 15:10:32 +0200

    cleanup

--- 1_elementary_tasks/2_envelope.py
+++
@@ -1,8 +0,0 @@
-while True:
-    a, b, c, d = [float(input(f'{e}: ')) for e in 'abcd']
-    if (a <= c and b <= d) or (b <= c and a <= d):
-        print(True)
-    else:
-        print(False)
-    if not input('continue? [y]es\n').lower() in ['y', 'yes']:
-        break
